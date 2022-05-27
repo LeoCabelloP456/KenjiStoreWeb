8590b3b5bf6bb3531d102ff6e9fbcf7359bb1bf5
Nicol Arancibia <narancibia@apside.cl>
2022-05-27 16:22:09 -0400

    Se arregla herencia de base.html hacia index.html

--- KenjiStoreWeb01/core/urls.py
+++ KenjiStoreWeb01/core/urls.py
@@ -1,18 +1,16 @@
 from django.urls import path
-from . import views
-
 from django.contrib import admin  
-from .views import catalogo, producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8
+from .views import index, registroPag, loginPag, catalogo, producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8
 
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path ('', index, name= "index"),
     path('catalogo/', catalogo, name = 'lista_catalogo'),
 
-    path('registro/', views.registroPag, name="registro"),
-    path('login/', views.loginPag, name="login"),
+    path('registro/', registroPag, name="registro"),
+    path('login/', loginPag, name="login"),
     path('producto1/', producto1, name="producto1"),
     path('producto2/', producto2, name="producto2"),
     path('producto3/', producto3, name="producto3"),
@@ -20,7 +18,7 @@ urlpatterns = [
     path('producto5/', producto5, name="producto5"),
     path('producto6/', producto6, name="producto6"),
     path('producto7/', producto7, name="producto7"),
-    path('producto8/', producto8, name="producto8"),
+    path('producto8/', producto8, name="producto")
 ]
 
  
