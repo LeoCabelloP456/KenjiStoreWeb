7ead1541cfaa25713adb4d2786c00c89fdbefe90
Leandro Cabello Puga <leandro.cabellogmail.com>
2022-06-09 16:26:04 -0400

    Template de checkout listo y funcionando

--- KenjiStoreWeb01/core/urls.py
+++ KenjiStoreWeb01/core/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('login/', views.loginPag, name="login"),
     path('logout/', views.logoutUser, name="logout"),
     path('cart/', views.cart, name="cart"),  
+    path('checkout/', views.checkout, name="checkout"),  
     #path('producto_ejemplo/', views.producto_ejemplo, name="producto_ejemplo"),
     #path('producto/<int:pk>', views.single_producto, name="single_producto"),
     
